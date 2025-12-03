7f1dd7936fd73b88e3dcbd6b245aee3c37a1bc02
Sebastian Müller <seb.mueller.91@web.de>
2025-12-03 07:10:41 +0100

    Solved day3-2

--- src/Day03.kt
+++ src/Day03.kt
@@ -15,7 +15,7 @@ fun main() {
     val input = readInput("Day${DAY.toDayString()}")
     part1(input).println()
 
-    check(part2(testInput) == 17524L)
+    check(part2(testInput) == 3121910778619L)
     part2(input).println()
 }
 
@@ -25,9 +25,8 @@ private fun List<Int>.highestJoltage(numberDigits: Int = 2): Long {
 
     for (i in numberDigits-1 downTo 0) {
         extractedDigits.add(curList.subList(0, curList.size - i).maxOf { it })
-
-        val indexOfFirstDigit = curList.indexOfFirst { extractedDigits.first() == it }
-        curList = curList.subList(indexOfFirstDigit + 1, curList.size)
+        val indexOfFirstOccurence = curList.indexOfFirst { extractedDigits.last() == it }
+        curList = curList.subList(indexOfFirstOccurence + 1, curList.size)
     }
 
     return extractedDigits.joinToString ("").toLong()
