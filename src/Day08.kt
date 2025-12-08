2c8642ea14b939296a485ba4c50e3e6080cf8145
Sebastian Müller <seb.mueller.91@web.de>
2025-12-08 18:59:36 +0100

    Continued day 8

--- src/Day08.kt
+++ src/Day08.kt
@@ -7,27 +7,26 @@ class Day08 {
 private const val DAY = 8
 
 fun main() {
-    fun part1(input: List<String>, iterations: Int): Int {
+    fun part1(input: List<String>, iterations: Int): Long {
         val junctions = input.parse()
         val distances = junctions.calculateDistances()
-        val circuits = mutableListOf(mutableSetOf<Junction>())
+        val circuits = mutableListOf<MutableSet<Junction>>()
 
-        for (iteration in 0..<iterations) {
+        repeat(iterations) {
             val minIndex = distances.getCoordinatesOfMin()
             circuits.addJunctions(junctions[minIndex.first], junctions[minIndex.second])
-            circuits.sortedBy { it.size }.take(3).map { it.size }.println()
-            println()
+            circuits.sortedByDescending { it.size }.map { it.size }.println()
         }
 
-        return circuits.sortedBy { it.size }.take(3).map { it.size }.reduce {acc, i -> acc*i}
+        return circuits.sortedByDescending { it.size }.take(3).map { it.size }.reduce {acc, i -> acc*i}.toLong()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.size.toLong()
     }
 
     val testInput = readInput("Day${DAY.toDayString()}_test")
-    check(part1(testInput, 10) == 40)
+    check(part1(testInput, 10) == 40L)
 
     val input = readInput("Day${DAY.toDayString()}")
     part1(input, 1000).println()
@@ -37,18 +36,11 @@ fun main() {
 private fun MutableList<MutableSet<Junction>>.addJunctions(junction1: Junction, junction2: Junction) {
     forEach { circuit ->
         if (circuit.contains(junction1) || circuit.contains(junction2)) {
-            "if".println()
-            circuit.println()
-            junction1.println()
-            junction2.println()
             circuit.add(junction1)
             circuit.add(junction2)
             return
         }
     }
-    "else".println()
-    junction1.println()
-    junction2.println()
     add(mutableSetOf(junction1, junction2))
 }
 
@@ -88,15 +80,15 @@ private fun List<String>.parse(): List<Junction> {
     val regex = Regex("""(\d+),(\d+),(\d+)""")
     forEach { line ->
         val (x, y, z) = regex.find(line)!!.destructured
-        list.add(Junction(x.toInt(), y.toInt(), z.toInt()))
+        list.add(Junction(x.toLong(), y.toLong(), z.toLong()))
     }
     return list
 }
 
 private data class Junction(
-    val x: Int,
-    val y: Int,
-    val z: Int
+    val x: Long,
+    val y: Long,
+    val z: Long
 ) {
     fun distanceTo(other: Junction): Double =
         sqrt((x - other.x).toDouble().pow(2) + (y - other.y).toDouble().pow(2) + (z - other.z).toDouble().pow(2))
